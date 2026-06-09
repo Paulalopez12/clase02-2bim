c39cd609a00040904a12e42e33e67b90d75ed7fc
Paulalopez12 <paulalopezacaro1201@gmail.com>
2026-06-09 16:30:46 -0500

    ya

--- orm/consulta1.py
+++ orm/consulta1.py
@@ -5,11 +5,11 @@ from modelo import engine, Serie
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Traemos todas las series
+
 series = session.query(Serie).all()
 
 for s in series:
-    # Llamamos únicamente al método de los actores
+    
     promedio = s.obtener_edad_actores()
     print(f"Serie: {s.titulo} - Promedio de edad: {promedio}")
 session.close()
